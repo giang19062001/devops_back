0fb95425bc7533a99bab2e926240efefdd768901
NGO HOANG GIANG <meobapgiang19062001@gmail.com>
2025-05-07 13:34:08 +0700

    revert database.js

--- config/database.js
+++ config/database.js
@@ -1,25 +1,25 @@
 //server current
-mysqlConfig = {
-    connectionLimit: 100,
-    host: "127.0.0.1",
-    user: "root",
-    password: "giang19062001",
-    port: 3306,
-    database: "projectdevops",
-    charset: "utf8",
-};
-
-
-//remote 
 // mysqlConfig = {
 //     connectionLimit: 100,
-//     host: "3.104.104.119",
-//     user: "shuket",
+//     host: "127.0.0.1",
+//     user: "root",
 //     password: "123456",
 //     port: 3306,
 //     database: "devops_testing",
 //     charset: "utf8",
 // };
+
+
+//remote 
+mysqlConfig = {
+    connectionLimit: 100,
+    host: "3.104.104.119",
+    user: "shuket",
+    password: "123456",
+    port: 3306,
+    database: "devops_testing",
+    charset: "utf8",
+};
 // mySQL pool
 let mysql = require("mysql2/promise");
 let mysqlPool = mysql.createPool(mysqlConfig);
